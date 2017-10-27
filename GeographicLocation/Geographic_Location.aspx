5143d29629821fadb3ff3f305370926c6556e2b0
Hasmik Mikaelyan <hasmikmikaelyan7@gmail.com>
2017-10-27 18:54:49 +0300

    First Commit.

---
+++ GeographicLocation/Geographic_Location.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Geographic_Location.aspx.cs" Inherits="GeographicLocation.Geographic_Location" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>IP To Country Example</title>
+</head>
+<body style = "font-family:Arial;font-size:10pt">
+    <form id="form2" runat="server">
+    <asp:GridView ID="gvLocation" runat="server" AutoGenerateColumns = "false">
+        <Columns>
+            <asp:BoundField DataField="IPAddress" HeaderText="IP Address" />
+            <asp:BoundField DataField="CountryName" HeaderText="Country" />
+            <asp:BoundField DataField="CityName" HeaderText="City" />
+            <asp:BoundField DataField="RegionName" HeaderText="Region" />
+            <asp:BoundField DataField="CountryCode" HeaderText="Country Code" />
+            <asp:BoundField DataField="Latitude" HeaderText="Latitude" />
+            <asp:BoundField DataField="Longitude" HeaderText="Latitude" />
+            <asp:BoundField DataField="Timezone" HeaderText="Timezone" />
+        </Columns>
+    </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ GeographicLocation/Geographic_Location.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GeographicLocation
+{
+    public partial class Geographic_Location : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ipAddress = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                ipAddress = Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            string APIKey = "<Your API Key>";
+            string url = string.Format("http://api.ipinfodb.com/v3/ip-city/?key={0}&ip={1}&format=json", APIKey, ipAddress);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                Location location = new JavaScriptSerializer().Deserialize<Location>(json);
+                List<Location> locations = new List<Location>();
+                locations.Add(location);
+                gvLocation.DataSource = locations;
+                gvLocation.DataBind();
+            }
+        }
+    }
+}
